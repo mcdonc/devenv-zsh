bd4584f5706ddf5fd56b7cc97d5f0a3f418a56eb
Chris McDonough <chrism@plope.com>
2025-05-03 22:41:33 -0400

    rename zsh.nix to default.nix

--- example/devenv.nix
+++ example/devenv.nix
@@ -3,10 +3,15 @@ let
   unstable = import nixpkgs-unstable {
     system = pkgs.stdenv.system;
   };
+  devenv-zsh = fetchTarball {
+    url = "https://github.com/mcdonc/devenv-zsh/archive/master.tar.gz";
+    sha256 = "sha256:0rkbhwbpirmm8g6qzcj7i8r3m5z1f8in5crmk3c7h896w3kw1g9x";
+  };
+    
 in
 
 {
-  imports = [ ../zsh.nix ];
+  imports = [ (import "${devenv-zsh}") ];
   processes.silly-example.exec = "while true; do echo hello && sleep 1; done";
   enterShell = ''
     echo "hello from bash at shell level $SHLVL!"
