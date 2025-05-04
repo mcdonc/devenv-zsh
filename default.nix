8ca5710dc80fd56ff5c7f592103940feefb90db9
Chris McDonough <chrism@plope.com>
2025-05-04 16:23:15 -0400

    remove debug

--- default.nix
+++ default.nix
@@ -82,9 +82,7 @@ in
     in
     lib.mkIf cfg.enable {
       enterShell = lib.mkAfter ''
-        set -x
-        echo "disable: $DEVENV_ZSH_DISABLE"
-        if [[ "$${DEVENV_ZSH_DISABLE:-}" != "0" && -n "$${DEVENV_ZSH_DISABLE:-}" ]]; then
+        if [[ "$${DEVENV_ZSH_DISABLE:-}" == "0" || -z "$${DEVENV_ZSH_DISABLE:-}" ]]; then
           # XXX hack: don't break "devenv up"
           if [ -z "$_DEVENV_ZSH_EXECED" ]; then
             export _DEVENV_ZSH_EXECED="$SHLVL"
