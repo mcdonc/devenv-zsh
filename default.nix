44234bcb1eadd37c55a80655d1817b972d054a42
Chris McDonough <chrism@plope.com>
2025-05-09 09:34:12 -0400

    get rid of overgeneral let

--- default.nix
+++ default.nix
@@ -1,31 +1,4 @@
 { pkgs, lib, config, inputs, ... }:
-let
-  strToBool = str: str != "";
-  cfg = config.zsh;
-  # use zshi instead of recreating it, although it is a bit slow
-  zshi = pkgs.stdenv.mkDerivation {
-    name="devenv-zsh-zshi";
-    src = pkgs.fetchFromGitHub {
-      owner = "romkatv";
-      repo = "zshi";
-      rev = "c9c90687448a1f0aae30d9474026de608dc90734";
-      sha256 = "sha256-OB96i93ZxKDgOqIFq1jM9l+wxAisRXtSCBcHbYDvxsI=";
-    };
-    installPhase = ''
-      mkdir -p $out/bin
-      cp zshi $out/bin/zshi
-      substituteInPlace $out/bin/zshi \
-        --replace '/usr/bin/env zsh' ${cfg.package}/bin/zsh
-    '';
-    meta = with lib; {
-      description = "ZSH -i except initial command exec'd after std zsh files";
-      homepage = "https://github.com/romkatv/zshi";
-      license = licenses.mit;
-      platforms = platforms.all;
-    };
-  };
-  extra-init = pkgs.writeText "devenv-zsh-extra-init" "${cfg.extraInit}";
-in
 {
   options.zsh = {
     enable = lib.mkOption {
@@ -41,22 +14,46 @@ in
     };
     extraInit = lib.mkOption {
       type = lib.types.str;
-      description = ''
-        ZSH-specific commands run after global ZSH initialilzation.
-     '';
+      description = "ZSH-specific commands run after global ZSH init.";
       default = "";
     };
   };
   config =
     let
+      cfg = config.zsh;
+      # use zshi instead of recreating it, although it is a bit slow
+      zshi = pkgs.stdenv.mkDerivation {
+        name="devenv-zsh-zshi";
+        src = pkgs.fetchFromGitHub {
+          owner = "romkatv";
+          repo = "zshi";
+          rev = "c9c90687448a1f0aae30d9474026de608dc90734";
+          sha256 = "sha256-OB96i93ZxKDgOqIFq1jM9l+wxAisRXtSCBcHbYDvxsI=";
+        };
+        installPhase = ''
+          mkdir -p $out/bin
+          cp zshi $out/bin/zshi
+          substituteInPlace $out/bin/zshi \
+            --replace '/usr/bin/env zsh' ${cfg.package}/bin/zsh
+        '';
+        meta = with lib; {
+          description = "ZSH -i but initial command exec'd after std zsh files";
+          homepage = "https://github.com/romkatv/zshi";
+          license = licenses.mit;
+          platforms = platforms.all;
+        };
+      };
+      strToBool = str: str != "";
+      extra-init = pkgs.writeText "devenv-zsh-extra-init" "${cfg.extraInit}";
       normal-zsh = ''${cfg.package}/bin/zsh -i'';
-      shimmed-zsh = ''${zshi}/bin/zshi ". ${extra-init}"'';
-      zsh-command = if strToBool(cfg.extraInit) then shimmed-zsh else normal-zsh;
+      shim-zsh = ''${zshi}/bin/zshi ". ${extra-init}"'';
+      zsh-command = if strToBool(cfg.extraInit) then shim-zsh else normal-zsh;
     in
-    lib.mkIf cfg.enable {
-      enterShell = lib.mkAfter ''
+      lib.mkIf cfg.enable {
+        enterShell = lib.mkAfter ''
         # not disabled
-        if [ "$DEVENV_ZSH_DISABLE" == "0" ] || [ -z "$DEVENV_ZSH_DISABLE" ];then
+        if [ "$DEVENV_ZSH_DISABLE " == "0" ] || \
+                [ -z "$DEVENV_ZSH_DISABLE" ]; then
           # XXX hack: don't break "devenv up"
           if [ -z "$_DEVENV_ZSH_EXECED" ]; then
             export _DEVENV_ZSH_EXECED="$SHLVL"
@@ -64,5 +61,5 @@ in
           fi
         fi
       '';
-    };
+      };
 }
